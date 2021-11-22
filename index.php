a7c6cd2f7841db613c74a067244b4e67d65b31ef
Charles Severance <csev@umich.edu>
2021-11-22 13:22:30 -0500

    Progress

--- index.php
+++ index.php
@@ -1,5 +1,47 @@
 <?php
+use \Tsugi\Util\Net;
+use \Tsugi\Core\LTIX;
+use \Tsugi\UI\Output;
 
-require_once "config.php";
+require "top.php";
+require "nav.php";
 
-header("Location: ".$CFG->wwwroot."/store");
+?>
+<div id="container">
+<!--
+<div style="margin-left: 10px; float:right">
+<iframe width="400" height="225" src="https://www.youtube.com/embed/flRUuodVPq0?rel=0" frameborder="0" allowfullscreen></iframe>
+</div>
+-->
+<h1><?= $CFG->servicedesc ?></h1>
+<p>
+This web site provides free / OER materials to help you learn the history and inner workings of the Internet.
+You can take this course and receive a certificate at:
+<ul>
+<li><a href="https://www.coursera.org/learn/internet-history" target="_blank">Coursera: <?= $CFG->servicedesc ?></a> </li>
+</ul>
+</p>
+<h2>Technology</h2>
+<p>
+This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a> 
+framework to embed a learning 
+management system into this site and handle the autograders.  
+If you are interested in collaborating
+to build these kinds of sites for yourself, please see the 
+<a href="http://www.tsugi.org" target="_blank">tsugi.org</a> website.
+<h3>Copyright</h3>
+<p>
+The material produced specifically for this site is by Charles Severance and others
+and is Copyright Creative Commons Attribution 3.0 
+unless otherwise indicated.  
+</p>
+<!--
+<?php
+echo("IP Address: ".Net::getIP()."\n");
+echo(Output::safe_var_dump($_SESSION));
+var_dump($USER);
+?>
+-->
+</div>
+<?php 
+require "foot.php";
