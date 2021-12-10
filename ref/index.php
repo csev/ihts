e9e2387e6530343c3bc5e51ef00d93f31aecfbb2
Charles Severance <csev@umich.edu>
2021-12-09 20:09:36 -0500

    Progress

---
+++ ref/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+ul, #ZUL {
+  list-style-type: none;
+}
+
+#ZUL {
+  margin: 0;
+  padding: 0;
+}
+
+/* Style the caret/arrow */
+.caret {
+  cursor: pointer;
+  user-select: none; /* Prevent text selection */
+}
+
+/* Create the caret/arrow with a unicode, and style it */
+.caret::before {
+  content: "\25B6";
+  color: black;
+  display: inline-block;
+  margin-right: 6px;
+}
+
+/* Rotate the caret/arrow icon when clicked on (using JavaScript) */
+.caret-down::before {
+  transform: rotate(90deg);
+}
+
+/* Hide the nested list */
+.nested {
+  display: none;
+}
+
+.active {
+  display: block;
+}
+</style>
+</head>
+<body>
+<?php
+// https://www.w3schools.com/howto/howto_js_treeview.asp
+
+require_once("../tsugi/config.php");
+
+$contents = file_get_contents("refs.json");
+$json = json_decode($contents);
+$array = (array) $json;
+
+echo("<p>IHTS References</p>\n");
+echo('<ul id="ZUL">'."\n");
+foreach ($array as $key => $value) {
+    if ( !is_array($value->links) || count($value->links) < 1 ) continue;
+    echo('<li><span class="caret">'.$value->title."</span></a>\n");
+    echo('<ul class="nested">'."\n");
+    foreach ($value->links as $link) {
+        $href = str_replace("{apphome}", $CFG->apphome, $link->href);
+        echo('<li><a href="'.$href.'" target="_blank">'.$link->title.'</a></li>'."\n");
+    }
+    echo('</ul></li>'."\n");
+}
+echo("<ul>\n");
+?>
+
+<script>
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+</script>
+
+</body>
+</html>
+
