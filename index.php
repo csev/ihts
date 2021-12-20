f73f9243ae02afebfcf0bdc56d61d845349ba0e6
Charles Severance <csev@umich.edu>
2021-12-20 13:26:18 -0500

    Updates.

--- index.php
+++ index.php
@@ -18,32 +18,35 @@ You can take this course and receive a certificate at:
 <ul>
 <li><a href="https://www.coursera.org/learn/internet-history" target="_blank">Coursera: <?= $CFG->servicedesc ?></a> </li>
 <li><a href="https://online.umich.edu/courses/internet-history-technology-and-security/" target="_blank">Free certificates for University of Michgan students and staff</a></li>
-<li>Free Code Camp</li>
-</ul>
+<li>
+<a href="https://www.youtube.com/watch?v=47NRaBVxgVM" target="_blank">Free Code Camp</a>
+provides this material for free in a single video so you can sit back
+and binge-watch the 9+ hours of material.
+</ul>.
 </p>
 <p>
-If you are watching the videos on a television, you might find our 
+If you are watching the videos on a television, you might find our
 <a href="ref" target="_blank">quick reference</a> useful on your phone or computer.
 </p>
 <p>
-If you 
+If you
 <a href="tsugi/login.php">log in</a> to this site you have joined a free, global open and online course. You have a grade book, autograded assignments and discussion forums.
-Since this site is used by learners, we take your 
+Since this site is used by learners, we take your
 <a href="privacy">privacy</a> very seriously.
 </p>
 <h2>Technology</h2>
 <p>
-This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a> 
-framework to embed a learning 
-management system into this site and handle the autograders.  
+This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a>
+framework to embed a learning
+management system into this site and handle the autograders.
 If you are interested in collaborating
-to build these kinds of sites for yourself, please see the 
+to build these kinds of sites for yourself, please see the
 <a href="http://www.tsugi.org" target="_blank">tsugi.org</a> website.
 <h3>Copyright</h3>
 <p>
 The material produced specifically for this site is by Charles Severance and others
-and is Copyright Creative Commons Attribution 3.0 
-unless otherwise indicated.  
+and is Copyright Creative Commons Attribution 3.0
+unless otherwise indicated.
 </p>
 <!--
 <?php
@@ -53,5 +56,5 @@ var_dump($USER);
 ?>
 -->
 </div>
-<?php 
+<?php
 require "foot.php";
