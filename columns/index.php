12a320357d8549a1f007f010123015df3717fef8
Charles Severance <csev@umich.edu>
2024-08-10 09:39:00 -0400

    Remove cookie reference

--- columns/index.php
+++ columns/index.php
@@ -3,7 +3,6 @@
 use \Tsugi\Util\U;
 
 require_once "../tsugi/config.php";
-require_once "../cookie.php";
 
 header("Cache-Control: no-transform, max-age=2592000");
 
@@ -13,8 +12,6 @@ if ( ! file_exists($document) ) {
     header("Location: ".$CFG->apphome);
     return;
 }
-
-if ( ! check_cookie($CFG->servicedesc, $document) ) return;
 
 $type = mime_content_type($document);
 $contents = file_get_contents($document);
